489b08ebc89fdb47b174f4e555d1d1baf0617658
Hannah McCullough <hmac091@gmail.com>
2022-02-15 12:10:15 -0700

    owner dash list created

---
+++ client/src/components/OwnerDashBookings.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function OwnerDashBookings() {
+
+    return (
+        <div>
+            OwnerDashBookings
+        </div>
+    )
+}
+
+export default OwnerDashBookings;
